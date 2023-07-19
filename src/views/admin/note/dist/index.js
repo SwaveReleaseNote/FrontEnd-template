45c8016340b2f15f407beaafdb369297362417cb
Gihyun Kim <83570546+kimgihean@users.noreply.github.com>
2023-07-19 16:15:29 +0900

    릴리즈노트 Text 블록 생성

--- src/views/admin/note/dist/index.js
+++ src/views/admin/note/dist/index.js
@@ -1,20 +1,77 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 exports.__esModule = true;
 var react_1 = require("react");
 var Version_1 = require("./components/Version");
 var Date_1 = require("./components/Date");
 var SideMenubar_1 = require("./components/SideMenubar");
-function index() {
+function Index() {
     var mockLatestVersion = "3.6.5";
+    var _a = react_1.useState([
+        { id: 1, content: "" },
+    ]), noteBlocks = _a[0], setNoteBlocks = _a[1];
+    var textRef = react_1.useRef(null);
+    react_1.useLayoutEffect(function () {
+        textRef.current.focus();
+    });
+    var addNewNoteBlock = function () {
+        setNoteBlocks(function (prev) {
+            return (__spreadArrays(prev, [{ id: prev[prev.length - 1].id + 1, content: '' }]));
+        });
+    };
+    var handleCreateBlockEnterKeyPress = function (event, id) {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            addNewNoteBlock();
+        }
+    };
+    var handleContentChange = function (event, id) {
+        var updatedNoteBlocks = noteBlocks.map(function (noteBlock) {
+            return noteBlock.id === id ? __assign(__assign({}, noteBlock), { content: event.target.value }) : noteBlock;
+        });
+        setNoteBlocks(updatedNoteBlocks);
+    };
+    // 엔터 누를 때 마다 textarea 나오도록 만들기
+    // const newNoteBlock = document.getElementById("note-block");
+    // function createNewNoteContent() {
+    //   const newNoteContent = document.createElement("textarea");
+    //   newNoteContent.classList.add("note-content");
+    //   newNoteBlock.appendChild(newNoteContent);
+    //   newNoteContent.focus();
+    // };
+    // document.addEventListener("keydown", function(event) {
+    //   if (event.key === "Enter") {
+    //     event.preventDefault();
+    //     createNewNoteContent();
+    //   }
+    // })
     return (react_1["default"].createElement("div", { className: "h-full grid-cols-1 gap-5 2xl:grid-cols-3" },
         react_1["default"].createElement("div", { className: "flex justify-between col-span-1 h-fit w-full xl:col-span-1 2xl:col-span-2" },
             react_1["default"].createElement("div", { className: 'mt-5 w-full' },
-                react_1["default"].createElement("textarea", { placeholder: 'test' })),
+                noteBlocks.map(function (noteBlock) { return (react_1["default"].createElement("textarea", { ref: textRef, id: "note-block", key: noteBlock.id, value: noteBlock.content, onChange: function (e) { return handleContentChange(e, noteBlock.id); }, onKeyDown: function (e) { return handleCreateBlockEnterKeyPress(e, noteBlock.id); } })); }),
+                react_1["default"].createElement("textarea", { id: "note-content", placeholder: 'test' })),
             react_1["default"].createElement(SideMenubar_1["default"], { width: 280 },
                 react_1["default"].createElement("div", { className: "mt-5 flex-row justify-center h-auto " },
                     react_1["default"].createElement(Version_1["default"], { latestVersion: mockLatestVersion }),
                     react_1["default"].createElement(Date_1["default"], null))))));
 }
-exports["default"] = index;
+exports["default"] = Index;
 
 //# sourceMappingURL=index.js.map
--- src/views/admin/note/index.tsx
+++ src/views/admin/note/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect, Component, KeyboardEvent, ChangeEvent, useLayoutEffect } from 'react'
 import NoteField from 'components/note/NoteField'
 import tableDataDevelopment from "../tables/variables/tableDataDevelopment";
 import tableDataCheck from "../tables/variables/tableDataCheck";
@@ -12,20 +12,85 @@ import Version from './components/Version';
 import Date from './components/Date';
 import SideMenubar from './components/SideMenubar';
 
-export default function index() {
+interface NoteBlock {
+  id: number;
+  content: string;
+}
+
+export default function Index() {
 
   const mockLatestVersion = "3.6.5";
-    
+  const [noteBlocks, setNoteBlocks] = useState<NoteBlock[]>([
+    {id:1, content: ""},
+  ]);
+  const textRef = useRef<HTMLTextAreaElement>(null);
+
+  useLayoutEffect(() => {
+    textRef.current.focus();
+  });
+
+  const addNewNoteBlock = () => {
+    setNoteBlocks((prev) => {
+      return (
+        [...prev, {id: prev[prev.length-1].id+1, content:''}] 
+      );
+    });
+  };
+  
+  const handleCreateBlockEnterKeyPress = (event: KeyboardEvent<HTMLTextAreaElement>, id: number) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      addNewNoteBlock();
+    }
+  }
+
+  const handleContentChange = (event: ChangeEvent<HTMLTextAreaElement>, id: number) => {
+    const updatedNoteBlocks = noteBlocks.map((noteBlock) =>
+      noteBlock.id === id ? { ...noteBlock, content: event.target.value } : noteBlock
+    );
+    setNoteBlocks(updatedNoteBlocks);
+  };
+
+  // 엔터 누를 때 마다 textarea 나오도록 만들기
+  // const newNoteBlock = document.getElementById("note-block");
+
+  // function createNewNoteContent() {
+  //   const newNoteContent = document.createElement("textarea");
+  //   newNoteContent.classList.add("note-content");
+
+  //   newNoteBlock.appendChild(newNoteContent);
+
+  //   newNoteContent.focus();
+  // };
+
+  // document.addEventListener("keydown", function(event) {
+  //   if (event.key === "Enter") {
+  //     event.preventDefault();
+  //     createNewNoteContent();
+  //   }
+  // })
+
   return (
     <div className="h-full grid-cols-1 gap-5 2xl:grid-cols-3">
       <div className="flex justify-between col-span-1 h-fit w-full xl:col-span-1 2xl:col-span-2">
         <div className='mt-5 w-full'>
-          <textarea placeholder='test'></textarea>
+          {noteBlocks.map((noteBlock) => (
+            <textarea
+            ref={textRef}
+            id="note-block"
+            key={noteBlock.id}
+            value={noteBlock.content}
+            onChange={(e) => handleContentChange(e, noteBlock.id)}
+            onKeyDown={(e) => handleCreateBlockEnterKeyPress(e, noteBlock.id)} />
+          ))
+
+          }
+          <textarea id="note-content" placeholder='test'></textarea>
         </div>
         {/* 버전, 날짜 생성 카드 */}
         <SideMenubar width={280}>
           <div className="mt-5 flex-row justify-center h-auto ">
-            <Version latestVersion={mockLatestVersion}/> 
+            <Version latestVersion={mockLatestVersion} />
             <Date />
           </div>
         </SideMenubar>
