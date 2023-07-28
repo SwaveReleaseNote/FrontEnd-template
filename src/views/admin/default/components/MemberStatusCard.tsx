bf7f9f04040c5b6d9b2c814d792d5f4c065c3113
root <tmdtjq0116@naver.com>
2023-07-28 16:15:17 +0900

    commit : Data 이름 변경
    type : change
    body : 로그인 로그아웃 상태 확인 변수명 변경

--- src/views/admin/default/components/MemberStatusCard.tsx
+++ src/views/admin/default/components/MemberStatusCard.tsx
@@ -10,7 +10,7 @@ type Props = {
 type MemberStatus = {
   memberId: number;
   memberName: string;
-  isOnline: Boolean;
+  online: Boolean;
 };
 
 const MemberStatusCard: React.FC<Props> = ({ projectId }) => {
@@ -32,18 +32,18 @@ const MemberStatusCard: React.FC<Props> = ({ projectId }) => {
         console.log("Mocking data");
 
         const mockResponse: MemberStatus[] = [
-          { memberId: 1, memberName: "함건욱", isOnline: true },
-          { memberId: 2, memberName: "김기현", isOnline: false },
-          { memberId: 3, memberName: "김성국", isOnline: false },
-          { memberId: 4, memberName: "강준희", isOnline: true },
-          { memberId: 5, memberName: "전강훈", isOnline: true },
-          { memberId: 6, memberName: "이승섭", isOnline: true },
-          { memberId: 7, memberName: "이승섭", isOnline: true },
-          { memberId: 8, memberName: "이승섭", isOnline: true },
-          { memberId: 9, memberName: "이승섭", isOnline: true },
-          { memberId: 10, memberName: "이승섭", isOnline: true },
-          { memberId: 11, memberName: "이승섭", isOnline: true },
-          { memberId: 12, memberName: "이승섭", isOnline: true },
+          { memberId: 1, memberName: "함건욱", online: true },
+          { memberId: 2, memberName: "김기현", online: false },
+          { memberId: 3, memberName: "김성국", online: false },
+          { memberId: 4, memberName: "강준희", online: true },
+          { memberId: 5, memberName: "전강훈", online: true },
+          { memberId: 6, memberName: "이승섭", online: true },
+          { memberId: 7, memberName: "이승섭", online: true },
+          { memberId: 8, memberName: "이승섭", online: true },
+          { memberId: 9, memberName: "이승섭", online: true },
+          { memberId: 10, memberName: "이승섭", online: true },
+          { memberId: 11, memberName: "이승섭", online: true },
+          { memberId: 12, memberName: "이승섭", online: true },
         ];
 
         setMemberStatus(mockResponse);
@@ -82,12 +82,12 @@ const MemberStatusCard: React.FC<Props> = ({ projectId }) => {
               <tr key={member.memberId}>
                 <td className="py-2">{member.memberName}</td>
                 <td className="flex items-center py-2 pl-[50px]">
-                  {member.isOnline ? "온라인" : "오프라인"}
+                  {member.online ? "온라인" : "오프라인"}
                 </td>
                 <td>
                   <div
                     className={`ml-5 h-4 w-4 rounded-full ${
-                      member.isOnline ? "bg-green-400" : "bg-red-400"
+                      member.online ? "bg-green-400" : "bg-red-400"
                     }`}
                   ></div>
                 </td>
