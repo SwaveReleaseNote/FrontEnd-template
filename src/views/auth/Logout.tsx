9ebe0b42dd4c4abfa9bedc15d449cd3adfc79c29
root <tmdtjq0116@naver.com>
2023-08-09 16:32:12 +0900

    commit : Logout, SignIn cookie 설정
    type : fix
    body : Logout, SignIn cookie 설정(제거,

--- src/views/auth/Logout.tsx
+++ src/views/auth/Logout.tsx
@@ -1,30 +1,31 @@
 import React, { useRef, useEffect, type ReactElement } from 'react';
 import type * as StompJs from '@stomp/stompjs';
-import { useNavigate } from "react-router-dom";
-
-
+import { useNavigate } from 'react-router-dom';
+import { removeCookie } from './cookie';
 const Logout = (): ReactElement => {
-  const navigate = useNavigate();
-  // Logout으로 넘어오는지 log 확인
-  console.log("sdafafsadfsads");
-  const client = useRef<StompJs.Client | null>(null);
+   const navigate = useNavigate();
+   // Logout으로 넘어오는지 log 확인
+   console.log('sdafafsadfsads');
+   const client = useRef<StompJs.Client | null>(null);
 
-  const disconnect = ():void => {
-    console.log('disconnect');
-    if ((client.current == null) || !client.current.connected) return;
-     // eslint-disable-next-line @typescript-eslint/no-floating-promises
-     client.current.deactivate();
+   const disconnect = (): void => {
+      console.log('disconnect');
+      if (client.current == null || !client.current.connected) return;
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      client.current.deactivate();
    };
 
-  useEffect(() => {
-    try {
-      disconnect();
-        window.localStorage.clear();
-        navigate("/auth/sign-in")
-    } catch (error) {
-      console.error(error);
-    }
-  }, []);
+   useEffect(() => {
+      try {
+         removeCookie('id',{path: '/'});
+         disconnect();
+         window.localStorage.clear();
+
+         navigate('/auth/sign-in');
+      } catch (error) {
+         console.error(error);
+      }
+   }, []);
 
    return <></>; // Placeholder return statement as the component doesn't render anything
 };
