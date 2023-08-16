28cfaaab19c70374af164872a5f905b88be23a43
Leesungsup <tmdtjq0116@naver.com>
2023-08-17 06:14:10 +0900

    Update stompClientUtils.tsx

--- src/views/auth/stompClientUtils.tsx
+++ src/views/auth/stompClientUtils.tsx
@@ -5,7 +5,7 @@ let client: StompJs.Client | null = null;
 
 export const createStompClient = (token: string): void => {
   client = new StompJs.Client({
-    brokerURL: 'ws://61.109.214.110:80/api/alert/ws-stomp',
+    brokerURL: 'ws://61.109.214.110:80/api/socket/ws-stomp',
     connectHeaders: {
       Authorization: token,
     },
