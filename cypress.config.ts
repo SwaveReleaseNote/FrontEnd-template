c74d24ad19844c9abfdcd603ff38622b7be9a5ef
ham <tomy8964@naver.com>
2023-08-18 11:35:18 +0900

    update

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   projectId: '8m1oyr',
    e2e: {
-      baseUrl: 'http://back-service:3000',
+      baseUrl: 'http://61.109.214.110:80',
       fixturesFolder: 'src/cypress/fixtures',
       supportFile: 'src/cypress/support/index.js',
       experimentalStudio: true,
